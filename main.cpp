9d86cb762406ff2d71709577bd9fee2cb94762c8
bitStream93 <tylerboland@protonmail.com>
2025-03-23 17:49:41 -0500

    added license header

--- main.cpp
+++ main.cpp
@@ -1,3 +1,34 @@
+/*
+* Copyright (C) 2025 bitStream
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * As a special requirement, any source code distribution must include the
+ * original license text and retain this notice.
+ *
+ * If you modify this Program, or any covered work, by linking or combining
+ * it with other code, such other code is not for that reason alone subject
+ * to any of the requirements of the GNU Affero GPL version 3.
+ *
+ * ADDITIONAL PERMISSION under the GNU Affero GPL version 3 section 7:
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with [name of library, name of software, etc],
+ * containing parts covered by the terms of [name of license],
+ * the licensors of this Program grant you additional permission
+ * to convey the resulting work.
+ */
+
 #include <windows.h>
 #include <algorithm>
 #include <array>
